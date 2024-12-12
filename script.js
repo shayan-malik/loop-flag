901e24ef3fe46c0886bbe8cad3732a712948ae41
Shayan Malik <shayaaanmalik@gmail.com>
2024-12-13 00:56:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function city()
+{
+    let cities = ["Karachi", "Islamabad", "Peshawar", "Lahore", "Quetta"];
+    let insertCity = document.getElementById("insertCity").value;
+    let cityMatch = "";
+
+    for (i=0; i < cities.length; i++)
+    {
+        if(insertCity.toLowerCase() == cities[i].toLowerCase())
+        {
+            cityMatch = "yes"
+        }
+    }
+
+    if(cityMatch == "yes")
+    {
+        alert("City Matched");
+    }
+    else{
+        alert("City Did Not Match")
+    }
+    
+}
